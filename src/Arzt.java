98bd79c12eb4c1a7d817b91b2a30a407015b6af4
Zoeylle <sekulic.zorica.a@gmail.com>
2026-01-14 11:00:43 +0100

    Die Klassen

---
+++ src/Arzt.java
@@ -0,0 +1,2 @@
+public class Arzt {
+}
---
+++ src/Termin.java
@@ -0,0 +1,32 @@
+public class Termin {
+}
+package model;
+
+import java.sql.Date;
+import java.sql.Time;
+
+public class Termin {
+    private int idTermin;
+    private Date datum;
+    private Time uhrzeit;
+    private String grund;
+    private String status;
+
+    // Die Verknüpfungen zu den anderen Tabellen
+    private int patientId;  //(FK)
+    private int arztId;    //(FK)
+
+    // Konstruktor
+    public Termin(int idTermin, Date datum, Time uhrzeit, String grund, String status, int patientId, int arztId) {
+        this.idTermin = idTermin;
+        this.datum = datum;
+        this.uhrzeit = uhrzeit;
+        this.grund = grund;
+        this.status = status;
+        this.patientId = patientId;
+        this.arztId = arztId;
+    }
+
+    // Hier drunter: Getter und Setter erstellen (Alt + Einfügen in IntelliJ)
+    // ...
+}
